190e57ac90e09e320802b75e3a5afa68da42ab05
oblivisheee <molniya213y@gmail.com>
2024-04-25 00:34:37 +0300

    v0.1.0

---
+++ src/api.rs
@@ -0,0 +1,41 @@
+use openai_api_rs::v1::api::Client;
+use openai_api_rs::v1::chat_completion::{self, ChatCompletionRequest};
+use rand::Rng;
+
+const GPT: &str = "gpt-3.5-turbo";
+const GEMINI: &str = "gemini-pro";
+const MISTRAL: &str = "mistral-large-2402";
+
+pub fn get_completion(client: Client, prompt: &str) -> String {
+    let mut rng = rand::thread_rng();
+
+    let random_number = rng.gen_range(1..=3);
+    let model;
+
+    if random_number == 1 {
+        model = GPT;
+    } else if random_number == 2 {
+        model = GEMINI;
+    } else {
+        model = MISTRAL;
+    }
+
+    let req = ChatCompletionRequest::new(
+        String::from(model), // Now model is accessible here
+        vec![chat_completion::ChatCompletionMessage {
+            role: chat_completion::MessageRole::user,
+            content: chat_completion::Content::Text(String::from(prompt)),
+            name: None,
+        }],
+    );
+
+    let result = client.chat_completion(req).unwrap();
+    result.choices[0].message.content.as_ref().unwrap().clone()
+}
+
+pub fn client(api: &str) -> Client {
+    let api_data = String::from(api);
+    const BASE_URL: &str = "https://api.naga.ac/v1";
+    let endpoint = std::env::var("OPENAI_API_BASE").unwrap_or_else(|_| BASE_URL.to_owned());
+    Client::new_with_endpoint(endpoint, api_data)
+}
---
+++ src/main.rs
@@ -0,0 +1,36 @@
+mod api;
+use eframe::egui;
+
+const API_KEY: &str = "6OXIyUHWb9ouesIAu7TX_8uNKCTEZ37yromakTvXops";
+
+#[derive(Default)]
+pub struct Calculator {
+    prompt: String,
+    ai_response: String,
+}
+
+impl eframe::App for Calculator {
+    fn update(&mut self, ctx: &egui::Context, _frame: &mut eframe::Frame) {
+        let client = api::client(&API_KEY);
+        egui::CentralPanel::default().show(ctx, |ui| {
+            ui.label("Calculate all you need!");
+
+            ui.text_edit_singleline(&mut self.prompt);
+            if ui.button("Ask (3 requests per min)").clicked() {
+                ui.label("Generating...");
+                self.ai_response = api::get_completion(client, &self.prompt);
+                ui.label(&self.ai_response);
+            }
+            ui.label(&self.ai_response);
+        });
+    }
+}
+
+fn main() {
+    let options = eframe::NativeOptions::default();
+    let _ = eframe::run_native(
+        "Calculator",
+        options,
+        Box::new(|_cc| Box::<Calculator>::default()),
+    );
+}
